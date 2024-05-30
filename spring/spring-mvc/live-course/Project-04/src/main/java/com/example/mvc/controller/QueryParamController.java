0fa74ca7329ff1e1a83595530fec3eb78139cf63
sagarshingare16 <sagargshingare16@gmail.com>
2024-05-30 23:51:56 +0530

    query and path param

--- spring/spring-mvc/live-course/Project-04/src/main/java/com/example/mvc/controller/QueryParamController.java
+++ spring/spring-mvc/live-course/Project-04/src/main/java/com/example/mvc/controller/QueryParamController.java
@@ -8,9 +8,12 @@ import java.util.Map;
 
 @Controller
 public class QueryParamController {
+
+    //localhost:8181/get?name=Sagar
+    //localhost:8181/get?name=Sagar&course=SpringBoot
     @GetMapping("/get")
     public String generateSomeMsg(@RequestParam("name")String n, String course, Map<String, Object> model) {
-        String msg="Hey There! "+n+ " , I hope Youre enjoying the journey of learning "+course;
+        String msg="Hey There! "+n+ " , I hope your are enjoying the journey of learning "+course;
 
         model.put("msg",msg);
         return "index";
