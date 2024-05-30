c7d098dcf331f89e20f3c304cf37e088e70f34f5
sagarshingare16 <sagargshingare16@gmail.com>
2024-05-30 23:57:58 +0530

    path param

---
+++ spring/spring-mvc/live-course/Project-04/src/main/java/com/example/mvc/controller/PathParamController.java
@@ -0,0 +1,19 @@
+package com.example.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+
+@Controller
+public class PathParamController {
+
+    //localhost:8181/getmessage/Rajesh
+    @GetMapping("/getMessage/{name}/{course}")
+    public String getSomeMsg(@PathVariable("name")String n, @PathVariable("course")String c, Map<String, Object> model) {
+        String msg="Hey, "+ n+ " You can join our live "+ c+ " course in future";
+        model.put("msg", msg);
+        return "index";
+    }
+}
