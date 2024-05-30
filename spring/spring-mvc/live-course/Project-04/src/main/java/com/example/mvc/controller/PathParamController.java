2695713e507bd86ad45deeb8ccbcd6d891e59871
sagarshingare16 <sagargshingare16@gmail.com>
2024-05-31 00:04:26 +0530

    app name and messages

--- spring/spring-mvc/live-course/Project-04/src/main/java/com/example/mvc/controller/PathParamController.java
+++ spring/spring-mvc/live-course/Project-04/src/main/java/com/example/mvc/controller/PathParamController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 @Controller
 public class PathParamController {
 
-    //localhost:8484/getMessage/Rajesh
+    //localhost:8484/getMessage/Sagar
     @GetMapping("/getMessage/{name}/{course}")
     public String getSomeMsg(@PathVariable("name")String n, @PathVariable("course")String c, Map<String, Object> model) {
         String msg="Hey, "+ n+ " You can join our live "+ c+ " course in future";
